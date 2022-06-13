301f483c436dbf7639607ca438a139f57d0f8e0a
Bashar Qaddumi <48452956+bqaddumi@users.noreply.github.com>
2022-06-13 16:19:28 +0300

    Fix style (#8)
    * add second footer section and fix some styles
    
    * remove un used code
    
    * update readme
    
    * add screenshot of dark desktop in read me file
    
    * fix image in readme
    
    * add screenshots for the dark, light, mobile, and desktop versions
    
    Co-authored-by: Bashar Qaddumi <bqaddumi@asaltech.com>

--- README.md
+++ README.md
@@ -12,4 +12,21 @@ then
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-[Production demo:](http://ecommerce-bashar-fe-task.surge.sh)
+Production Demo: [http://ecommerce-bashar-fe-task.surge.sh](http://ecommerce-bashar-fe-task.surge.sh)
+
+Desktop Dark:
+
+![Desktop Dark](https://i.postimg.cc/RFnRTgV0/desktop-Dark.jpg)
+
+
+Desktop Light:
+
+![Desktop Light](https://i.postimg.cc/VvQFZXdW/desktop-Light.jpg)
+
+Mobile Dark:
+
+![Mobile Dark](https://i.postimg.cc/vTrtJDnF/Mobile-Dark.jpg)
+
+Mobile Light:
+
+![Mobile Light](https://i.postimg.cc/3rm14fcS/Mobile-Light.jpg)
---
+++ src/components/footer/footerLinks.tsx
@@ -0,0 +1,74 @@
+import { Box, Typography, Grid } from "@mui/material";
+import { isMobile } from "react-device-detect";
+import { Instagram, Google, Twitter, Facebook } from "@mui/icons-material";
+
+import {
+  containerStyle,
+  grayColor,
+  firstSection,
+  secondSection,
+  alignTextRight,
+  alignTextLeft,
+  secondSectionTitle,
+} from "./footerLinks-style";
+
+type FooterLinksProps = {};
+
+const FooterLinks: React.FC<FooterLinksProps> = (props: FooterLinksProps) => {
+  const gridLayout = (x: number) => (isMobile ? 12 : x);
+  return (
+    <Box sx={containerStyle}>
+      <Grid container spacing={2} sx={firstSection}>
+        <Grid xs={gridLayout(3)} sx={alignTextLeft}>
+          <Typography sx={grayColor}> Our Office Address</Typography>
+          <Typography> 169 Florida Ave, L.A City </Typography>
+        </Grid>
+        <Grid xs={gridLayout(3)} sx={alignTextLeft}>
+          <Typography sx={grayColor}> Please call us</Typography>
+          <Typography> (+84) 12346869669 </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={gridLayout(6)}
+          sx={isMobile ? alignTextLeft : alignTextRight}
+        >
+          <Instagram /> <Google /> <Facebook /> <Twitter />
+        </Grid>
+      </Grid>
+      <Box sx={secondSection}>
+        <Grid container sx={{}}>
+          <Grid xs={gridLayout(3)} sx={alignTextLeft}>
+            <Typography sx={secondSectionTitle}> Our Stores </Typography>
+            <Typography sx={grayColor}> Liverpool City</Typography>
+          </Grid>
+          <Grid xs={gridLayout(3)} sx={alignTextLeft}>
+            <Typography sx={secondSectionTitle}> My Account </Typography>
+            <Typography sx={grayColor}> My Cart</Typography>
+            <Typography sx={grayColor}> Check Out</Typography>
+            <Typography sx={grayColor}> Wishlist</Typography>
+            <Typography sx={grayColor}> Team & Policy</Typography>
+            <Typography sx={grayColor}> Your Account</Typography>
+          </Grid>
+          <Grid item xs={gridLayout(3)} sx={alignTextLeft}>
+            <Typography sx={secondSectionTitle}> Information </Typography>
+            <Typography sx={grayColor}> Shipping & Return</Typography>
+            <Typography sx={grayColor}> Gift Cards</Typography>
+            <Typography sx={grayColor}> Track My Order</Typography>
+            <Typography sx={grayColor}> Team & Policy</Typography>
+            <Typography sx={grayColor}> FAQs</Typography>
+          </Grid>
+          <Grid item xs={gridLayout(3)} sx={alignTextLeft}>
+            <Typography sx={secondSectionTitle}> How to Buy </Typography>
+            <Typography sx={grayColor}> Making Payments</Typography>
+            <Typography sx={grayColor}> Delivary Options</Typography>
+            <Typography sx={grayColor}> Buyer Protection</Typography>
+            <Typography sx={grayColor}> New User Guide</Typography>
+            <Typography sx={grayColor}> Partner Ship</Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+
+export default FooterLinks;
