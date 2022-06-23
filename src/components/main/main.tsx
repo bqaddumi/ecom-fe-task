9c584b416afdea34ccb227642f9dc5270548ec36
Bashar Qaddumi <bqaddumi@asaltech.com>
2022-06-23 16:18:30 +0300

    remove unused code

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { Box, Stack, ListItem } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
-import { useProducts } from "../../shared/productsContext";
-import { getProducts } from "../../axios";
+
 import Header from "../header/header";
 import Navbar from "../navbar/navbar";
 import Footer from "../footer/footer";
@@ -11,22 +10,11 @@ import CartPage from "../../pages/cartPage/cartPage";
 import ShopPage from "../../pages/shopPage/shopPage";
 import FooterLinks from "../footer/footerLinks";
 import ProductPage from "../../pages/productPage/productPage";
-import { ProductType } from "../../types";
 
 type MainProps = {};
 
 const Main: React.FC<MainProps> = (props: MainProps) => {
   const ref = useRef<HTMLDivElement>(null);
-
-  // const { dispatch } = useProducts(); //{type:"set", products: products}
-
-  // useEffect(() => {
-  //   getProducts().then((res: { data: ProductType[] }) => {
-  //     const { data } = res;
-  //     dispatch({ type: "set", products: data });
-  //   });
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
 
   const scrollToTop = () => {
     ref.current?.scrollTo(0, 0);
